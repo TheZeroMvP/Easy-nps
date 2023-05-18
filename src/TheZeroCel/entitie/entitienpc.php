17535636ba01018d55662d42392f34b1e727e594
TheZeroMvP <115753420+TheZeroMvP@users.noreply.github.com>
2023-05-18 11:29:30 -0500

    Update entitienpc.php

--- src/TheZeroCel/entitie/entitienpc.php
+++ src/TheZeroCel/entitie/entitienpc.php
@@ -1,1 +1,18 @@
+<?php
 
+namespace TheZeroCel\entitie;
+
+use pocketmine\block\VanillaBlocks;
+use pocketmine\entity\Human;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\item\VanillaItems;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\DoubleTag;
+use pocketmine\nbt\tag\FloatTag;
+use pocketmine\nbt\tag\ListTag;
+use pocketmine\player\Player;
+use pocketmine\utils\TextFormat;
+
+class NpcHuman extends Human
+{
