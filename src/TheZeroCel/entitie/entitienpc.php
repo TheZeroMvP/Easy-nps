c1dfd914b94d8166570fac47d3bcc2eabbbb2387
TheZeroMvP <115753420+TheZeroMvP@users.noreply.github.com>
2023-05-18 11:31:41 -0500

    Update entitienpc.php

--- src/TheZeroCel/entitie/entitienpc.php
+++ src/TheZeroCel/entitie/entitienpc.php
@@ -16,3 +16,71 @@ use pocketmine\utils\TextFormat;
 
 class NpcHuman extends Human
 {
+    /** @var int|null */
+
+    /**
+     * @param Player $player
+     *
+     * @return CosmeticsNPCEntity
+     */
+    public static function create(Player $player): self
+    {
+        $nbt = CompoundTag::create()
+            ->setTag("Pos", new ListTag([
+                new DoubleTag($player->getLocation()->x),
+                new DoubleTag($player->getLocation()->y),
+                new DoubleTag($player->getLocation()->z)
+            ]))
+            ->setTag("Motion", new ListTag([
+                new DoubleTag($player->getMotion()->x),
+                new DoubleTag($player->getMotion()->y),
+                new DoubleTag($player->getMotion()->z)
+            ]))
+            ->setTag("Rotation", new ListTag([
+                new FloatTag($player->getLocation()->yaw),
+                new FloatTag($player->getLocation()->pitch)
+            ]));
+        return new self($player->getLocation(), $player->getSkin(), $nbt);
+    }
+
+    /**
+     * @param int $currentTick
+     *
+     * @return bool
+     */
+    public function onUpdate(int $currentTick): bool
+    {
+        $parent = parent::onUpdate($currentTick);
+        $this->setNameTag(TextFormat::colorize("§l§a* NEW *\n&r&6 &9NpcHuman \n&r&o&7/NpcHuman"));
+        $this->setNameTagAlwaysVisible(true);
+
+        return $parent;
+    }
+
+    /**
+     * @param EntityDamageEvent $source
+     */
+    public function attack(EntityDamageEvent $source): void
+    {
+        $source->cancel();
+
+        if (!$source instanceof EntityDamageByEntityEvent) {
+            return;
+        }
+
+        $damager = $source->getDamager();
+
+        if (!$damager instanceof Player) {
+            return;
+        }
+
+        if ($damager->getInventory()->getItemInHand()->getId() === 276) {
+            if ($damager->hasPermission("NpcHuman.permiso")) {
+                $this->kill();
+            }
+            return;
+        }
+
+        NpcHumanMenu::create($damager);
+    }
+}
